9a61f4fc9cc737a24b0329a460a695a33090685e
dw3nt <rick@dualwieldent.com>
2021-12-06 10:03:19 -0700

    day 5 part 1 solution

--- day5/part1.js
+++ day5/part1.js
@@ -1,3 +1,40 @@
 const { input } = require("./input");
 
-console.log(input);
+var ventPos = {};
+
+function getVentPosKey(coord) {
+    return coord[0].toString() + "." + coord[1].toString();
+}
+
+function increaseCountAtPosition(ventPosKey) {
+    if (! (getVentPosKey(ventPosKey) in ventPos )) {
+        ventPos[getVentPosKey(ventPosKey)] = 0;
+    }
+    ventPos[getVentPosKey(ventPosKey)] += 1;
+}
+
+input.forEach((coordPair) => {
+    let startPos = coordPair[0];
+    let endPos = coordPair[1];
+
+    if(startPos[0] === endPos[0]) { // row matches
+        for(let i = 0; i <= Math.abs(endPos[1] - startPos[1]); i++) {
+            increaseCountAtPosition( [startPos[0],  startPos[1] + (i * Math.sign(endPos[1] - startPos[1])) ] );
+        }
+
+    } else if(startPos[1] === endPos[1]) { // col matches
+        for(let i = 0; i <= Math.abs(endPos[0] - startPos[0]); i++) {
+            increaseCountAtPosition( [ startPos[0] + (i * Math.sign(endPos[0] - startPos[0])), startPos[1] ] );
+        }
+    }
+});
+
+let dangerSpotCount = 0;
+let ventPosVals = Object.values(ventPos);
+for(let i=0; i<ventPosVals.length; i++) {
+    if (ventPosVals[i] >= 2) {
+        dangerSpotCount += 1;
+    }
+}
+
+console.log(dangerSpotCount);
