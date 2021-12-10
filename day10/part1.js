376bc657480d6bd7869769b44d8a98b314160446
dw3nt <rick@dualwieldent.com>
2021-12-10 09:36:15 -0700

    day 10 part 1 solution

--- day10/part1.js
+++ day10/part1.js
@@ -1,3 +1,33 @@
 const { input } = require("./input");
+const openCloseMap = { '(': ')', '[': ']', '{': '}', '<': '>' };
+const illegalCharPoints = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
 
-console.log(input);
+let syntaxScore = 0;
+let invalidsFound = [];
+let openChunks = [];
+
+input.forEach((line) => {
+    openChunks = [];
+    for(let i=0; i<line.length; i++) {
+        let char = line.charAt(i);
+        if (Object.keys(openCloseMap).includes(char)) {
+            openChunks.push(char);
+        } else if (Object.values(openCloseMap).includes(char)) {
+            if (openChunks.length > 0) {
+                let lastOpen = openChunks.pop();
+                if (openCloseMap[lastOpen] != char) {
+                    invalidsFound.push(char);
+                    break;
+                }
+            } else {
+                console.log("Can't start with a closing character: " + char);
+            }
+        }
+    }
+});
+
+invalidsFound.forEach((char) => {
+    syntaxScore += illegalCharPoints[char];
+});
+
+console.log(syntaxScore);
