d89b55f569c3b2206347a2a52a3a6f0893bb3f47
dw3nt <rick@dualwieldent.com>
2021-12-11 12:49:14 -0700

    day 11 part 2 solution

---
+++ day11/part2.js
@@ -0,0 +1,65 @@
+const { input } = require("./input");
+
+var flashes = 0;
+
+function flashAtPos(pos) {
+    input[pos[0]][pos[1]] = -1;
+    flashes++;
+    for (let rowOffset = -1; rowOffset < 2; rowOffset++) {
+        for (let colOffset = -1; colOffset < 2; colOffset++) {
+            let newRow = pos[0] + rowOffset;
+            let newCol = pos[1] + colOffset;
+
+            if (newRow >= 0 && newRow < 10 && newCol >= 0 && newCol < 10) {
+                if (input[newRow][newCol] > -1) {
+                    input[newRow][newCol] += 1;
+                    if (input[newRow][newCol] > 9) {
+                        flashAtPos([newRow, newCol]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function isFlashSynced() {
+    for (let row=0; row<input.length; row++) {
+        for (let col=0; col<input[0].length; col++) {
+            if (input[row][col] != 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+let flashSynced = false;
+let step = 0;
+while(!flashSynced) {
+    for (let row=0; row<input.length; row++) {
+        for (let col=0; col<input[0].length; col++) {
+            if (input[row][col] > -1) {
+                input[row][col] += 1;
+                if (input[row][col] > 9) {
+                    flashAtPos([row, col]);
+                    input[row][col] = -1;
+                }
+            }
+        }
+    }
+
+    for (let row=0; row<input.length; row++) {
+        for (let col=0; col<input[0].length; col++) {
+            if (input[row][col] == -1) {
+                input[row][col] = 0;
+            }
+        }
+    }
+
+    flashSynced = isFlashSynced();
+
+    step++;
+}
+
+console.log(step);
