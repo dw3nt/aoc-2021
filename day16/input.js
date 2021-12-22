d3daea91b7dbbb962951b61c9988b3d13fd3e3b2
dw3nt <rick@dualwieldent.com>
2021-12-22 08:00:17 -0700

    day 16 setup

---
+++ day16/input.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+let inputFile = (args.length >= 1 ? args[0] : "input.txt");
+
+const input = fs.readFileSync(path.resolve(__dirname, inputFile), "utf-8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return "0";
+    }
+
+    return data;
+}).toString().trim().split("\n");
+
+module.exports ={
+    input
+};
